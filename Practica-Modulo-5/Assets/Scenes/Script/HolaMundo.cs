a9a417c4a61e99f3ee614b23bfa3c3279ab5912b
IMatecocido <camilotrep@gmail.com>
2024-05-27 22:21:39 -0300

    HolaMundo2
    Debug.log("Algo Paso");
    Debug.log("Algo Salio Medianamente Mal");
    Debug.logerror("Algo Salio Muy Mal");

--- Practica-Modulo-5/Assets/Scenes/Script/HolaMundo.cs
+++ Practica-Modulo-5/Assets/Scenes/Script/HolaMundo.cs
@@ -7,7 +7,10 @@ public class HolaMundo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("HolaMundo");
+       
+        Debug.Log("Algo Paso");
+        Debug.LogWarning("Algo Salio Mal");
+        Debug.LogError("Algo Salio Muy Mal");
     }
 
     // Update is called once per frame
